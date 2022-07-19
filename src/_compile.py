0cb757e5ca37dd61b2010ddfef58c720a8ddbbcc
yodeng <yodeng@tju.edu.cn>
2022-07-19 15:44:48 +0800

    first commit

---
+++ src/_compile.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import glob
+import shutil
+import logging
+import fnmatch
+
+import multiprocessing as mp
+
+from .utils import *
+
+
+class CompileProject(object):
+
+    py_ext = ".py"
+
+    def __init__(self, project_dir="", compile_dir="", exclude_dir=None, exclude_file=None, interpreter=sys.executable, threads=1, c=False):
+        self.pdir = os.path.abspath(project_dir)
+        self.cdir = os.path.abspath(compile_dir)
+        self.threads = threads
+        if not os.path.isdir(self.cdir):
+            os.makedirs(self.cdir)
+        if os.path.isfile(self.pdir):
+            cpcmd = "cp %s %s" % (self.pdir, self.cdir)
+        else:
+            cpcmd = "cp -r %s/* %s" % (
+                self.pdir, self.cdir)
+        call(cpcmd)
+        self.compile_file = []
+        self.edir = [] if exclude_dir is None else exclude_dir
+        self.efile = [] if exclude_file is None else exclude_file
+        self.interpreter = interpreter
+        self.cc = c
+        self.tmdir = []
+
+    def list_compile_files(self):
+        for p, ds, fs in os.walk(self.cdir):
+            dn = os.path.basename(p)
+            for dp in self.edir:
+                if fnmatch.fnmatch(dn, dp):
+                    break
+            else:
+                for fp in self.efile:
+                    for fn in fnmatch.filter(fs, fp):
+                        fs.remove(fn)
+                for f in fs:
+                    if f.endswith(self.py_ext):
+                        self.compile_file.append(os.path.join(p, f))
+
+    def compile(self, pyf, remove=True):
+        self.logger.info("start compile %s file", pyf)
+        with tempdir() as td:
+            self.tmdir.append(td)
+            tf = os.path.join(td, "ccbuild.py")
+            self.write_setup(tf)
+            cmd = [self.interpreter, tf, pyf]
+            call(cmd, shell=False, msg=pyf, c=self.cc,
+                 tmpdir=self.tmdir)
+        outfile = glob.glob(os.path.splitext(
+            pyf)[0] + ".cpython*.so") or glob.glob(os.path.splitext(pyf)[0] + ".so")
+        if len(outfile) == 0:
+            return
+        elif len(outfile) != 1:
+            raise IOError("multi *.so file: %s" % outfile)
+        outfile = outfile[0]
+        if remove:
+            p = outfile.rsplit(".", 2)
+            n, ext = p[0], p[-1]
+            new_file_name = os.path.join(os.path.dirname(pyf), n+"."+ext)
+            shutil.move(outfile, new_file_name)
+            os.remove(os.path.splitext(new_file_name)[0] + ".c")
+            if os.path.isfile(pyf[:-3] + ".so"):
+                os.remove(pyf)
+            if os.path.isfile(pyf[:-3] + ".c"):
+                os.remove(pyf[:-3] + ".c")
+        self.logger.info("finished %s", pyf)
+
+    def __call__(self, pyfile):
+        return self.compile(pyfile, remove=True)
+
+    def compile_all(self, remove=True):
+        p = mp.Pool(self.threads)
+        p.map(self, self.compile_file)
+        return
+
+    def clean_source(self):
+        pass
+
+    def write_setup(self, outfile):
+        with open(outfile, "w") as fo:
+            # fo.write("#!%s\n" % self.interpreter)
+            ctx = text_wrap('''
+                import sys
+                import os
+                from setuptools import setup
+                from Cython.Build import cythonize
+
+                py_file = sys.argv[1]
+                sys.argv = sys.argv[:1]
+                setup(ext_modules=cythonize(
+                    py_file, language_level=str(sys.version_info.major), 
+                    force=True,
+                    nthreads=10,
+                    quiet = True),
+                    script_args=["build_ext", 
+                        "-b", os.path.dirname(py_file), 
+                        "-t", os.path.dirname(__file__)]
+                    )                                      
+                ''')
+            fo.write(ctx.strip() + "\n")
+
+    @property
+    def logger(self):
+        return mp.get_logger()
---
+++ src/utils.py
@@ -0,0 +1,208 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import os
+import sys
+import time
+import shutil
+import logging
+import argparse
+import tempfile
+import textwrap
+import contextlib
+import subprocess
+
+import multiprocessing as mp
+
+from setuptools import setup
+
+from types import MethodType
+from Cython.Build import cythonize
+
+from ._version import __version__
+
+PY = sys.version_info
+
+if PY.major == 2:
+    from copy_reg import pickle
+elif PY.major == 3:
+    from copyreg import pickle
+
+
+def clean_process(tmpdir=[]):
+    for tdir in tmpdir:
+        if os.path.isdir(tdir):
+            shutil.rmtree(tdir)
+    try:
+        for tdir in tmpdir._address_to_local:
+            if os.path.isdir(os.path.dirname(tdir)):
+                shutil.rmtree(os.path.dirname(tdir))
+    except:
+        pass
+    p = os.getpid()
+    g = os.getpgid(p)
+    os.killpg(g, 15)
+
+
+def pickle_method(method):
+    func_name = method.im_func.__name__
+    obj = method.im_self
+    cls = method.im_class
+    return unpickle_method, (func_name, obj, cls)
+
+
+def unpickle_method(func_name, obj, cls):
+    for cls in cls.mro():
+        try:
+            func = cls.__dict__[func_name]
+        except KeyError:
+            pass
+        else:
+            break
+    return func.__get__(obj, cls)
+
+
+def text_wrap(text):
+    return textwrap.dedent(text).strip()
+
+
+@contextlib.contextmanager
+def tempdir(*args, **kwargs):
+    tmpdir = tempfile.mkdtemp(*args, **kwargs)
+    try:
+        yield tmpdir
+    finally:
+        try:
+            shutil.rmtree(tmpdir)
+        except:
+            pass
+
+
+def call(cmd, run=True, verbose=False, shell=True, msg="", c=False, tmpdir=[]):
+    log = mp.get_logger()
+    if not run:
+        if verbose:
+            print(cmd)
+        return
+    if verbose:
+        try:
+            subprocess.check_call(cmd, shell=shell, stdout=sys.stdout,
+                                  stderr=sys.stderr)
+        except Exception as err:
+            if msg:
+                log.error("compile error %s" % msg)
+            if not c:
+                clean_process(tmpdir)
+    else:
+        with open(os.devnull, "w") as fo:
+            try:
+                subprocess.check_call(cmd, shell=shell, stdout=fo, stderr=fo)
+            except Exception as err:
+                if msg:
+                    log.error("compile error %s" % msg)
+                if not c:
+                    clean_process(tmpdir)
+
+
+def check_cython(python_exe):
+    proc = subprocess.Popen([python_exe, "-m", "cython"],
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    out = stdout+stderr
+    if "--version" in out.decode():
+        return True
+    return False
+
+
+def Mylog(logfile=None, multi=False, level="info"):
+    if multi:
+        logger = mp.get_logger()
+    else:
+        logger = logging.getLogger()
+    if level.lower() == "info":
+        logger.setLevel(logging.INFO)
+        f = logging.Formatter(
+            '[%(levelname)s %(asctime)s] %(message)s')
+    elif level.lower() == "debug":
+        logger.setLevel(logging.DEBUG)
+        f = logging.Formatter(
+            '[%(levelname)s %(threadName)s %(asctime)s %(funcName)s(%(lineno)d)] %(message)s')
+    if logfile is None:
+        h = logging.StreamHandler(sys.stdout)  # default: sys.stderr
+    else:
+        h = logging.FileHandler(logfile, mode='w')
+    h.setFormatter(f)
+    logger.addHandler(h)
+    return logger
+
+
+def format_exclude(input_exc=None):
+    out = []
+    if input_exc is not None:
+        for n, d in enumerate(input_exc[:]):
+            d = os.path.basename(os.path.normpath(d))
+            if d:
+                out.append(d)
+    return out
+
+
+def Argparse():
+    parser = argparse.ArgumentParser(
+        description="For compile *.py projects to *.so")
+    parser.add_argument("-p", "--python", type=str,
+                        help="python interpreter path to run you project after compile, %s by default" % sys.executable, default=sys.executable, metavar="<file>")
+    parser.add_argument("-i", "--input", type=str,
+                        help="input file or directory for compile", required=True, metavar="<file/dir>")
+    parser.add_argument("-o", "--output", type=str, help="output compiled directory", required=True,
+                        metavar="<dir>")
+    parser.add_argument("-t", "--threads", type=int, help="thread core, 5 by default", default=5,
+                        metavar="<int>")
+    parser.add_argument("-c", "--compile-continue", action='store_true', default=False,
+                        help="if compile fail, continue, default: exit program")
+    parser.add_argument("--exclude-dir", type=str, help="skipped directorys(basename only), simple regular expression allowed('./?/*'), multi input can be separated by whitespace",
+                        nargs="*", metavar="<dir>")
+    parser.add_argument("--exclude-file", type=str, help="skipped files(basename only), simple regular expression allowed('./?/*'), multi input can be separated by whitespace",
+                        nargs="*", metavar="<file>")
+    parser.add_argument('-v', '--version',
+                        action='version', version="v" + __version__)
+    args = parser.parse_args()
+    args.exclude_dir = format_exclude(args.exclude_dir) + ["__pycache__"]
+    args.exclude_file = format_exclude(args.exclude_file) + ["__init__.py"]
+    if args.python and which(args.python):
+        args.python = which(args.python)
+    return args
+
+
+def canonicalize(path):
+    return os.path.abspath(os.path.expanduser(path))
+
+
+def which(program, paths=None):
+    found_path = None
+    fpath, fname = os.path.split(program)
+    if fpath:
+        program = canonicalize(program)
+        if is_exe(program):
+            found_path = program
+    else:
+        paths_to_search = []
+        if isinstance(paths, (tuple, list)):
+            paths_to_search.extend(paths)
+        else:
+            env_paths = os.environ.get("PATH", "").split(os.pathsep)
+            paths_to_search.extend(env_paths)
+
+        for path in paths_to_search:
+            exe_file = os.path.join(canonicalize(path), program)
+            if is_exe(exe_file):
+                found_path = exe_file
+                break
+    return found_path
+
+
+def is_exe(file_path):
+    return (
+        os.path.exists(file_path)
+        and os.access(file_path, os.X_OK)
+        and os.path.isfile(os.path.realpath(file_path))
+    )
