33b5615e8d4bfb0cbe6212f7e3862d3812ebdb01
dengyong <dengyong@hugobiotech.com>
2022-07-21 10:17:18 +0800

    update copy function and some typo

--- src/_compile.py
+++ src/_compile.py
@@ -18,16 +18,11 @@ class CompileProject(object):
 
     def __init__(self, project_dir="", compile_dir="", exclude_dir=None, exclude_file=None, interpreter=sys.executable, threads=1, c=False):
         self.pdir = os.path.abspath(project_dir)
+        if not os.path.exists(self.pdir):
+            self.logger.error("No such file or directory: %s", self.pdir)
+            sys.exit(1)
         self.cdir = os.path.abspath(compile_dir)
         self.threads = threads
-        if not os.path.isdir(self.cdir):
-            os.makedirs(self.cdir)
-        if os.path.isfile(self.pdir):
-            cpcmd = "cp %s %s" % (self.pdir, self.cdir)
-        else:
-            cpcmd = "cp -r %s/* %s" % (
-                self.pdir, self.cdir)
-        call(cpcmd)
         self.compile_file = []
         self.edir = [] if exclude_dir is None else exclude_dir
         self.efile = [] if exclude_file is None else exclude_file
@@ -35,6 +30,12 @@ class CompileProject(object):
         self.cc = c
 
     def list_compile_files(self):
+        if os.path.isfile(self.pdir):
+            copy(self.pdir, self.cdir)
+        elif os.path.isdir(self.pdir):
+            mkdir(self.cdir)
+            cpcmd = "cp -r %s/* %s" % (self.pdir, self.cdir)
+            call(cpcmd)
         for p, ds, fs in os.walk(self.cdir):
             dn = os.path.basename(p)
             for dp in self.edir:
@@ -48,7 +49,7 @@ class CompileProject(object):
                     if f.endswith(self.py_ext):
                         self.compile_file.append(os.path.join(p, f))
 
-    def compile(self, pyf, remove=True):
+    def compile(self, pyf):
         self.logger.info("start compile %s file", pyf)
         fin = False
         with tempdir() as td:
@@ -57,10 +58,11 @@ class CompileProject(object):
             tf = os.path.join(td, "ccbuild.py")
             self.write_setup(tf)
             cmd = [self.interpreter, tf, pyf]
-            so = call(cmd, out=True, shell=False, msg=pyf, c=self.cc, tmdir=td)
-            if so:
+            _so = call(cmd, out=True, shell=False,
+                       msg=pyf, c=self.cc, tmdir=td)
+            if _so:
                 new_file_name = pyf[:-3] + ".so"
-                shutil.move(so, new_file_name)
+                shutil.move(_so, new_file_name)
                 if os.path.isfile(new_file_name):
                     os.remove(pyf)
                 fin = True
@@ -68,9 +70,11 @@ class CompileProject(object):
             self.logger.info("finished %s", pyf)
 
     def __call__(self, pyfile):
-        return self.compile(pyfile, remove=True)
+        return self.compile(pyfile)
 
-    def compile_all(self, remove=True):
+    def compile_all(self):
+        if len(self.compile_file) == 0:
+            self.list_compile_files()
         mg = mp.Manager()
         self.lock = mg.Lock()
         self.tmdir = mg.list()
@@ -118,14 +122,14 @@ class CompileProject(object):
                 py_file = os.path.abspath(sys.argv[1])
                 sys.argv = sys.argv[:1]
                 setup(ext_modules=cythonize(
-                    py_file, language_level=str(sys.version_info.major), 
+                    py_file, language_level=str(sys.version_info.major),
                     force=True,
                     nthreads=10,
-                    quiet = True),
-                    script_args=["build_ext", 
-                        "-b", os.path.dirname(__file__), 
-                        "-t", os.path.dirname(__file__)]
-                    )                                      
+                    quiet=True),
+                    script_args=["build_ext",
+                                 "-b", os.path.dirname(__file__),
+                                 "-t", os.path.dirname(__file__)]
+                )
                 ''')
             fo.write(ctx.strip() + "\n")
 
